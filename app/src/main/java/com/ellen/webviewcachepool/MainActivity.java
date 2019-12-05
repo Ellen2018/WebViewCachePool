8e8e613ef550390e31a71149756f000241a2b39b
陈浪 <chenlang@sx985.com>
2019-12-05 20:31:13 +0800

    更新WebViewCachePool用法，请看主Module app的MainActivity查看用法

--- app/src/main/java/com/ellen/webviewcachepool/MainActivity.java
+++ app/src/main/java/com/ellen/webviewcachepool/MainActivity.java
@@ -3,12 +3,33 @@ package com.ellen.webviewcachepool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
+
+import com.ellen.library.WebViewCachePool;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private RelativeLayout rlWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rlWebView = findViewById(R.id.rl_webview);
+        //获取一个WebView
+        webView = WebViewCachePool.getInstance(this).getWebView();
+        //动态添加到某个布局中
+        rlWebView.addView(webView);
+        //可直接加载地址
+        webView.loadUrl("https://www.baidu.com");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //回收或者销毁当前WebView
+        WebViewCachePool.getInstance(this).destory(webView,null);
     }
 }
